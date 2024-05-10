f62ae356431caec7525b42d0f027ab34f7aa6e89
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-09 23:05:32 -0400

    updated cartitem code

--- client/src/components/CartItem/index.jsx
+++ client/src/components/CartItem/index.jsx
@@ -1,69 +1,50 @@
-import { useStoreContext } from "../../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
-import { idbPromise } from "../../utils/helpers";
+import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  removeFromCart,
+  updateCartQuantity,
+} from "../store/actions/cartActions";
 
 const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
 
-  const [, dispatch] = useStoreContext();
-
-  const removeFromCart = item => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id
-    });
-    idbPromise('cart', 'delete', { ...item });
-
+  const removeFromCartHandler = () => {
+    dispatch(removeFromCart(item._id));
   };
 
-  const onChange = (e) => {
+  const updateQuantityHandler = (e) => {
     const value = e.target.value;
-    if (value === '0') {
-      dispatch({
-        type: REMOVE_FROM_CART,
-        _id: item._id
-      });
-      idbPromise('cart', 'delete', { ...item });
-
+    if (value === "0") {
+      dispatch(removeFromCart(item._id));
     } else {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: item._id,
-        purchaseQuantity: parseInt(value)
-      });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
-
+      dispatch(updateCartQuantity(item._id, parseInt(value)));
     }
-  }
+  };
 
   return (
     <div className="flex-row">
       <div>
-        <img
-          src={`/images/${item.image}`}
-          alt=""
-        />
+        <img src={`/images/${item.image}`} alt="" />
       </div>
       <div>
-        <div>{item.name}, ${item.price}</div>
+        <div>
+          {item.name}, ${item.price}
+        </div>
         <div>
           <span>Qty:</span>
           <input
             type="number"
             placeholder="1"
             value={item.purchaseQuantity}
-            onChange={onChange}
+            onChange={updateQuantityHandler}
           />
-          <span
-            role="img"
-            aria-label="trash"
-            onClick={() => removeFromCart(item)}
-          >
+          <span role="img" aria-label="trash" onClick={removeFromCartHandler}>
             🗑️
           </span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default CartItem;

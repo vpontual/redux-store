69df54436211819246e2fe29a9d72c3f7778c5d7
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-09 23:07:14 -0400

    updated nav code

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,20 +1,26 @@
-import Auth from "../../utils/auth";
+import React from "react";
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../store/actions/authActions";
 
 function Nav() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-  function showNavigation() {
-    if (Auth.loggedIn()) {
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  const showNavigation = () => {
+    if (isLoggedIn) {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
+            <Link to="/orderHistory">Order History</Link>
           </li>
           <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
+            <a href="/" onClick={logoutHandler}>
               Logout
             </a>
           </li>
@@ -24,32 +30,27 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       );
     }
-  }
+  };
 
   return (
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
+          <span role="img" aria-label="shopping bag">
+            🛍️
+          </span>
           -Shop-Shop
         </Link>
       </h1>
-
-      <nav>
-        {showNavigation()}
-      </nav>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
